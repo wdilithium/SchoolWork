7c7ec7ece857a2a347f954944048ef8429e7a893
wdilithium <43236841+wdilithium@users.noreply.github.com>
2018-09-14 19:51:52 +0800

    地址打印

---
+++ location.java
@@ -0,0 +1,10 @@
+public class location
+ { 
+     public static void main(String[] args)
+       {
+          int n=99;
+          System.out.print(System.identityHashCode(n));
+         }
+   } 
+
+  /*½á¹û166239592*/
