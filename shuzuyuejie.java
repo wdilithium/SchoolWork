cf80cbde815b084871c94a62a3dc098fc01e387b
wdilithium <43236841+wdilithium@users.noreply.github.com>
2018-09-14 19:50:25 +0800

    数组越界

---
+++ shuzuyuejie.java
@@ -0,0 +1,11 @@
+public class shuzuyuejie
+ { 
+     public static void main(String[] args)
+       {
+          int [][] array = {{1,2,3},{4,5}};
+          System.out.println(array[1][2]);
+       }
+ }
+/*½á¹û:Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 2
+
+ at shuzuyuejie.main(shuzuyuejie.java:6)*/
